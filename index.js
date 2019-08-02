6bae351b86edcd2798d027d9e7c7ef6e4ec0762b
tikalaka <baydadan001@gmail.com>
2019-08-02 15:06:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ client.once('ready' ,() => {
     console.log('Ready')
 });
 
-client.login('NjA2ODYxNzI5NDc3MDMzOTg0.XURSKQ.RVSHI2PNhhWWuVJ2t6Mxk6n0rM4');
+client.on('message', message => { 
+    console.log(message.content);
+    if(message.content === '!creeper'){
+        message.channel.send('AWW MAN!');
+    }
+})
+
+client.login('NjA2ODYxNzI5NDc3MDMzOTg0.XURUIg.9W5fzCMchPWVNL-8g4upcfGT_r4');
