d161451c6f53fd8ad2b8de10767d427ca7a497e5
tikalaka <baydadan001@gmail.com>
2019-08-08 07:52:35 -0600

    ADDED IMPORTANT INFORMATION

--- commands/creeper.js
+++ commands/creeper.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'creeper',
-    description: 'A great 2011 Minecraft parody reference',
+    description: 'A great 2011 Minecraft parody reference which has garnered millions of view of youtube even surpassing the original song',
     execute(message, args) {
         message.channel.send('AWW MAN!');
     },
