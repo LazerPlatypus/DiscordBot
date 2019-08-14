8e90e7135a69da47ad8f9c001ec703cf1a084d2b
Adam Holt (Laptop) <aholt@student.neumont.edu>
2019-08-14 08:42:39 -0600

    adds show-image
    the show image command allows the user to display uploaded images using the bot.

--- commands/upload.js
+++ commands/upload.js
@@ -3,19 +3,61 @@ module.exports = {
     description: 'allows the user to upload files to the upload folder' +
     'from inside discord',
     execute(message, args) {
+        // load dependencies
         const Discord = require('discord.js'); // gets the discord api running
         const fs = require('fs'); // loads the filesystem
         const download = require('../helperMethods/download.js'); // gets the helper method this class needs
 
+        // check for help, dont run any other code if true.
+        if (args[0] == '?') {
+            message.channel.send('This command allows for the uploading of images and sounds.\n' +
+            'Sounds can be retrieved using the \'play\' command\n ' +
+            'Images can be retrieved using the \'show-image\' command.\n' +
+            'usage: click the \'upload\' button, add the comment "!upload {desired name of file}", click send.')
+        }
+
         // currently discord only allows 1 attachment per message.
         // but we put the attachments in an array b/c it makes it easier to access
         var Attachment = (message.attachments).array(); // makes an array of attachments from the message.
+
+        // if there are no attachments, tell the user but dont run anything else.
+        if (Attachment[0] == undefined) {
+            message.channel.send('I didn\'t see any files uploaded!');
+            return;
+        }
+
         var fileURL = Attachment[0].url; // gets the url of the first attachments
 
-        // tells the user that their upload was successfull
-        download.execute(fileURL, `uploads\\${args[0]}.${fileURL.split('.').pop()}`, function () {
-            message.channel.send(`Upload completed, the name is **${args[0]}**`);
-            console.log(`new file uploaded, the name is ${args[0]}.${fileURL.split('.').pop()}`);
+        // make the name of the file by concating all the args together with escaped spaces
+        var fileName = args.join('\ ');
+
+        if (fileName.match('\.')) {
+            message.channel.send('the filename cannot contain a .');
+            return;
+        }
+
+        // get the file extension
+        var fileExtension = fileURL.split('.').pop();
+
+        // list of the accepted image and sound extensions
+        const imageExtensions = ['tif', 'tiff', 'bmp', 'jpg', 'jpeg', 'gif', 'png'];
+        const soundExtensions = ['mp3', 'ogg', 'aac'];
+        // list of places to put sounds or images
+        const locations = ['uploads', 'uploads\\sounds', 'uploads\\images'];
+
+        // the location of this file
+        var location = locations[0];
+        // set the location according to the filetype
+        if (imageExtensions.includes(fileExtension)) {
+            location = locations[2];
+        } else if (soundExtensions.includes(fileExtension)) {
+            location = locations[1];
+        }
+
+        // download the image to its location with the filename specified
+        download.execute(fileURL, `${location}\\${fileName}.${fileExtension}`, function () {
+            message.channel.send(`Upload completed, the name is **${fileName}**`);
+            console.log(`new file uploaded, the name is ${fileName}.${fileExtension}`);
         })
     },
 };
